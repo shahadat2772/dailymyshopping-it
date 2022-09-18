deb5dbc8e8837a2f6b40bf1a0d3c48a43a1b4390
shahadat2772 <shahadat2772@gmailcom>
2022-09-18 08:45:11 +0600

    initiate signup login shop components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import Shop from "./components/Shop";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <div className="App ">
       {/* Nav */}
       <Navbar />
-      <div className="max-w-7xl mx-auto"></div>
+      <div className="max-w-7xl mx-auto">
+        <Routes>
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/SignUp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <div>HELLO this is SIGN UP</div>;
+};
+
+export default SignUp;
