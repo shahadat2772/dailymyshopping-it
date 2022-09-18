4cc9aeb72d6ca0a8769bf3423af97966b18df7bf
shahadat2772 <shahadat2772@gmailcom>
2022-09-18 15:37:58 +0600

    fixed not updating cart issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -8,9 +8,39 @@ import Navbar from "./components/Navbar";
 import RequireAuth from "./components/RequireAuth";
 import Shop from "./components/Shop";
 import SignUp from "./components/SignUp";
+import p1 from "../src/productsimg/product-1.jpg";
+import p2 from "../src/productsimg/product-2.jpg";
+import p3 from "../src/productsimg/product-3.jpg";
+import { getCart, addToDb, removeFromDb } from "../src/utilities/fakedb";
 
 function App() {
   const [cart, setCart] = useState([]);
+
+  const products = [
+    { name: "Rubik's cube", img: p1, price: 34, id: 1 },
+    { name: "Mastermorphix", img: p2, price: 18, id: 2 },
+    { name: "Mirror puzzle", img: p3, price: 39, id: 3 },
+  ];
+
+  // Setting the cart items from local storage
+  const setItemsFromDb = () => {
+    let savedItems = [];
+    const savedProducts = getCart();
+    for (const id in savedProducts) {
+      const savedItem = products.find((product) => product.id == id);
+      if (savedItem) {
+        let quantity = savedProducts[id];
+        savedItem["quantity"] = quantity;
+        savedItems.push(savedItem);
+      }
+    }
+    setCart(savedItems);
+  };
+
+  useEffect(() => {
+    setItemsFromDb();
+  }, []);
+
   return (
     <div className="App">
       {/* Nav */}
@@ -21,7 +51,7 @@ function App() {
             path="/shop"
             element={
               <RequireAuth>
-                <Shop setCart={setCart} />
+                <Shop setItemsFromDb={setItemsFromDb} products={products} />
               </RequireAuth>
             }
           />
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,35 +1,7 @@
 import React, { useEffect, useState } from "react";
-import p1 from "../productsimg/product-1.jpg";
-import p2 from "../productsimg/product-2.jpg";
-import p3 from "../productsimg/product-3.jpg";
 import { getCart, addToDb, removeFromDb } from "../utilities/fakedb";
 
-const Shop = ({ setCart }) => {
-  const products = [
-    { name: "Rubik's cube", img: p1, price: 34, id: 1 },
-    { name: "Mastermorphix", img: p2, price: 18, id: 2 },
-    { name: "Mirror puzzle", img: p3, price: 39, id: 3 },
-  ];
-
-  // Setting the cart items from local storage
-  const setItemsFromDb = () => {
-    let savedItems = [];
-    const savedProducts = getCart();
-    for (const id in savedProducts) {
-      const savedItem = products.find((product) => product.id == id);
-      if (savedItem) {
-        let quantity = savedProducts[id];
-        savedItem["quantity"] = quantity;
-        savedItems.push(savedItem);
-      }
-    }
-    setCart(savedItems);
-  };
-
-  useEffect(() => {
-    setItemsFromDb();
-  }, []);
-
+const Shop = ({ products, setItemsFromDb }) => {
   return (
     <div className="productsContainer max-w-7xl mx-auto grid gap-6 px-2 grid-cols-2 lg:grid-cols-3 md:grid-cols-3 justify-items-center mt-12">
       {products.map((product) => (
