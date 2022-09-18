be8f698134b5a5ce36f972be5d17ed67bbc9682d
shahadat2772 <shahadat2772@gmailcom>
2022-09-18 09:12:48 +0600

    updated styles for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,41 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div className="bg-base-100">
       <div className="navbar max-w-7xl mx-auto">
         <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          <a className="normal-case text-xl">
+            Daily<span className="text-primary">Shopping</span>
+          </a>
         </div>
         <div className="flex-none">
-          <ul className="menu menu-horizontal p-0">
+          <ul className="menuItems menu-horizontal p-0">
             <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <a>
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2 bg-base-100">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              <NavLink
+                to={"/shop"}
+                className={({ isActive }) =>
+                  isActive
+                    ? "font-medium text-primary"
+                    : "font-medium hover:text-primary duration-300"
+                }
+              >
+                Shop
+              </NavLink>
             </li>
             <li>
-              <a>Item 3</a>
+              <NavLink
+                to={"/login"}
+                className={({ isActive }) =>
+                  isActive
+                    ? "font-medium text-primary"
+                    : "font-medium hover:text-primary duration-300"
+                }
+              >
+                Login
+              </NavLink>
             </li>
           </ul>
         </div>
---
+++ src/components/RequireAuth.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RequireAuth = () => {
+  return <div>hello</div>;
+};
+
+export default RequireAuth;
